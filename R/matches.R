fc12148642878c5c5f8ad3d921c69d372e8e83dc
jamann426 <jordan.amann@fema.dhs.gov>
2022-07-26 15:57:07 -0400

    new

--- R/matches.R
+++ R/matches.R
@@ -16,13 +16,15 @@
 #' @export
 #'
 uss_make_matches <- function(data_engsoc, country) {
-  
-  # validate
 
+  # validate
+  validate_data_frame(data_engsoc)
+  validate_cols(data_engsoc, cols_engsoc())
+  `%>%` <- magrittr::`%>%`
   # put into "standard" form
   result <-
-    data_engsoc |>
-    tibble::as_tibble() |>
+    data_engsoc %>%
+    tibble::as_tibble() %>%
     dplyr::transmute(
       country = as.character(.env$country),
       tier = factor(.data$tier, levels = c("1", "2", "3", "4")),
@@ -33,6 +35,6 @@ uss_make_matches <- function(data_engsoc, country) {
       goals_home = as.integer(.data$hgoal),
       goals_visitor = as.integer(.data$vgoal)
     )
-  
+
   result
 }
